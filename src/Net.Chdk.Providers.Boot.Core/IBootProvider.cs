c4e86dbd4652091c14fa3cb837593bc6bc8ca4c2
Dmitry Shechtman <damnian@gmail.com>
2017-05-11 02:42:59 +0300

    Add Offsets and Prefix

--- src/Net.Chdk.Providers.Boot.Core/IBootProvider.cs
+++ src/Net.Chdk.Providers.Boot.Core/IBootProvider.cs
@@ -5,6 +5,9 @@ namespace Net.Chdk.Providers.Boot
     public interface IBootProvider
     {
         string FileName { get; }
+        int[][] Offsets { get; }
+        byte[] Prefix { get; }
+
         IDictionary<int, byte[]> GetBytes(string fileSystem);
     }
 }
