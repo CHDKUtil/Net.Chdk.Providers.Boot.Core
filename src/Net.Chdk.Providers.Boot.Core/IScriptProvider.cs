4778e0cd9c7517a462bc02d2fff21601e26dc310
Dmitry Shechtman <damnian@gmail.com>
2017-05-26 15:50:05 +0300

    Add IScriptProvider

---
+++ src/Net.Chdk.Providers.Boot.Core/IScriptProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Net.Chdk.Providers.Boot
+{
+    public interface IScriptProvider
+    {
+        IDictionary<int, byte[]> GetBytes(string fileSystem);
+        IDictionary<string, byte[]> GetFiles();
+    }
+}
